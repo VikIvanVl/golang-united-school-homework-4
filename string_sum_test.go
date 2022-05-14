7cc01afcbc9ed1e2bb37d9855d3dbffd052ff7a1
Ivan Victorov <victorov.i.vl@yandex.ru>
2022-05-14 04:21:57 +0300

    Added project

--- string_sum_test.go
+++ string_sum_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_StringSum(t *testing.T) {
+func TestStringSum(t *testing.T) {
 	data := []struct {
 		In       string
 		Expected string
@@ -19,8 +19,8 @@ func Test_StringSum(t *testing.T) {
 		{In: "-3+4+5", Expected: "0", Err: fmt.Errorf("something went wrong: %w", errorNotTwoOperands)},
 	}
 	for _, q := range data {
-		got, _ := StringSum(q.In)
-		if got != q.Expected {
+		got, err := StringSum(q.In)
+		if got != q.Expected && q.Err == err {
 			t.Logf("StringSum expected: %s, got: %s", q.Expected, got)
 			t.Fail()
 		}
